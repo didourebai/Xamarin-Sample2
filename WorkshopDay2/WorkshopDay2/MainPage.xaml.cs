09d79378bbde47cef5923ee59606b002d1665b30
Rebai Hamida <didourebai@gmail.com>
2017-11-07 16:21:23 +0100

    first commit

---
+++ WorkshopDay2/WorkshopDay2/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkshopDay2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHorizontalStartClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Start;
+        }
+        private void OnHorizontalCenterClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Center;
+        }
+        private void OnHorizontalEndClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.End;
+        }
+        private void OnHorizontalFillClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Fill;
+        }
+
+
+        private void OnVerticalStartClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Start;
+        }
+
+        private void OnVerticalCenterClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Center;
+        }
+
+        private void OnVerticalEndClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.End;
+        }
+
+        private void OnVerticalFillClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Fill;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
